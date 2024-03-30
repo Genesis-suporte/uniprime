f0cdbe541d3da520f0f6335470dc5a73e1bd21ca
Tyaraju Terra <tyarajuterra@hotmail.com>
2024-03-30 01:23:50 -0300

    Finalizando página Cooperativismo financeiro

--- wp-content/themes/uniprime/blocks/bloco-beneficios-coop.php
+++ wp-content/themes/uniprime/blocks/bloco-beneficios-coop.php
@@ -3,44 +3,46 @@
   $titulo = get_field('titulo', $block['id']);
   $descricao = get_field('descricao', $block['id']);
 ?>
-<div class="bloco-beneficios-coop z-13">
+<section class="bloco-beneficios-coop z-13">
   <div class="container">
     <div class="row d-flex justify-content-between flex-column flex-lg-row">
-      <div class="label-block">
-        <?php echo esc_html($label); ?>
-      </div>
-      <div class="title-block">
-        <?php echo esc_html($titulo); ?>
-      </div>
-      <div class="container">
-        <div class="cards-bbc d-grid">
-          <?php 
-            if( have_rows('bloco-beneficios-coop') ) {
-              while ( have_rows('bloco-beneficios-coop') ) : the_row();
-                if( get_row_layout() == 'cards' ) {
-                  $classe = get_sub_field('classe');
-                  $titulo = get_sub_field('titulo');
-                  $descricao = get_sub_field('descricao');?>
-                  <div class="card-bbc">
-                    <div class="content-card">
-                      <div class="title icon-menu icon-logo">
-                        <?php echo esc_html($titulo); ?>
-                      </div>
-                      <div class="description">
-                        <?php echo __($descricao); ?>
+      <div class="content-beneficios-coop">
+        <div class="label-block">
+          <?php echo esc_html($label); ?>
+        </div>
+        <div class="title-block">
+          <?php echo esc_html($titulo); ?>
+        </div>
+        <div class="container">
+          <div class="cards-bbc d-grid">
+            <?php 
+              if( have_rows('bloco-beneficios-coop') ) {
+                while ( have_rows('bloco-beneficios-coop') ) : the_row();
+                  if( get_row_layout() == 'cards' ) {
+                    $classe = get_sub_field('classe');
+                    $titulo = get_sub_field('titulo');
+                    $descricao = get_sub_field('descricao');?>
+                    <div class="card-bbc">
+                      <div class="content-card">
+                        <div class="title icon-menu icon-logo">
+                          <?php echo esc_html($titulo); ?>
+                        </div>
+                        <div class="description">
+                          <?php echo __($descricao); ?>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <?php
-                }
-                
-              endwhile;
-            } else {
-              // Do something...
-            }
-          ?>
+                    <?php
+                  }
+                  
+                endwhile;
+              } else {
+                // Do something...
+              }
+            ?>
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
+</section>
