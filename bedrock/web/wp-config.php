5ea43b21b24a09543fced478a5c605ff9622eebc
Tyaraju Terra <tyarajuterra@hotmail.com>
2024-11-12 19:31:11 -0300

    Ajustes pro bedrock funcionar no repositório

--- bedrock/web/wp-config.php
+++ bedrock/web/wp-config.php
@@ -14,7 +14,7 @@ define( 'WP_ALLOW_MULTISITE', true );
 define( 'MULTISITE', true );
 define( 'SUBDOMAIN_INSTALL', false );
 $base = '/';
-define( 'DOMAIN_CURRENT_SITE', 'novouniprime.local' );
+define( 'DOMAIN_CURRENT_SITE', 'uniprime.local' );
 define( 'PATH_CURRENT_SITE', '/' );
 define( 'SITE_ID_CURRENT_SITE', 1 );
 define( 'BLOG_ID_CURRENT_SITE', 1 );
